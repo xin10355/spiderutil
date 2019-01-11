e462da63e5669d6fd81b8f48e40440675c1f52f3
xin10355 <1035507498@qq.com>
2019-01-12 00:26:30 +0800

    异常处理

--- src/main/java/com/spider/util/JdJingShui.java
+++ src/main/java/com/spider/util/JdJingShui.java
@@ -39,8 +39,8 @@ import java.util.Map;
 public class JdJingShui {
 
     static Logger log = Logger.getLogger(Alibaba.class);
-    static String JDBC_DRIVER = "org.apache.hive.jdbc.HiveDriver";
-    static String CONNECTION_URL = "jdbc:hive2://10.138.23.145:21050/;auth=noSasl";
+    static String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static String CONNECTION_URL = "jdbc:mysql://localhost:3306/test?useUnicode=true&characterEncoding=UTF-8";
     static Connection con = null;
     static WebClient wc = null;
     static String _p = "&sort=sort_rank_asc&trans=1&md=1&my=list_brand";
@@ -90,17 +90,17 @@ public class JdJingShui {
     public void save() throws Exception {
         log.info("save...");
         String now = "'" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "',"; //当前时间
-        String insert_sql = "INSERT into rrs.jddsr(created_date,name,shop_name,shop_url,filter_url,value,trend) VALUES (";
+        String insert_sql = "INSERT into jddsr(created_date,name,shop_name,shop_url,filter_url,value,trend) VALUES (";
         PreparedStatement ps = null;
         ResultSet st = null;
         try {
             Class.forName(JDBC_DRIVER);
-            con = DriverManager.getConnection(CONNECTION_URL, "shadmin", "shadmin");
+            con = DriverManager.getConnection(CONNECTION_URL, "root", "10355");
             int num = 1;
             for(Map.Entry<String, JdBrand> entry: brands.entrySet()) {
                 JdBrand brand = entry.getValue();
-                log.info("第" + num + "个：");
-                brand = filterBrand(entry.getValue());
+                log.info("第" + num + "个：" + brand.getName());
+                brand = filterBrand(brand);
                 num++;
                 try {
                     //insert
@@ -156,7 +156,7 @@ public class JdJingShui {
     public JdBrand filterBrand(JdBrand jdBrand) throws Exception {
         String brandName = jdBrand.getName();
         String aliasName = brandName; //别称
-        if(brandName.indexOf("（")>0){ //是否包含别称
+        if(brandName.indexOf("（")>0 && brandName.indexOf("）")>0){ //是否包含别称
             aliasName = brandName.substring(brandName.indexOf("（")+1, brandName.indexOf("）"));
             brandName = brandName.substring(0, brandName.indexOf("（"));
         }
@@ -188,7 +188,6 @@ public class JdJingShui {
             if(venderid.indexOf("10000")>=0 || shopName.contains("自营") || shopName.contains("专卖") || shopName.contains("专营") || shopName.contains("官方")){
                 continue; //跳过官方店、自营店、专营店、专卖店
             } else if((shopName.contains(brandName) || shopName.contains(aliasName))
-                    && (shopName.contains("净水") || shopName.contains("饮水"))
                     && shopName.contains("旗舰店")) {
                 //'品牌名'+ 类别 +'旗舰店'
                 jdBrand.setShopName(shopName);
@@ -206,30 +205,32 @@ public class JdJingShui {
      * @param jdBrand
      */
     public JdBrand getBrandInfo(JdBrand jdBrand) throws Exception {
-        if(jdBrand.getShopUrl()==null){
-            return jdBrand;
-        }
-        String url = jdBrand.getShopUrl().replace("index", "shopLevel"); //替换链接
-        HtmlPage jdympage = (HtmlPage) wc.getPage(url);
-        String appId = jdympage.querySelector("#pageInstance_appId").getAttributes().getNamedItem("value").getNodeValue();
-        //dsr header dom
-        HtmlPage dsrpage = wc.getPage(dsr_url + appId + "&_=" + new Date().getTime());
-        String jsonText = dsrpage.getBody().asXml();
-        String html = jsonText.replace("  ","").replace("\r\n","").replace("\\&quot;","");
-        Document dom = Jsoup.parse(html);
-        Elements spans = dom.getElementById("jRatingTotal_table").child(2).child(1).getElementsByTag("span");
-        if(spans.size()==2){ //与同行业相比有差别
-            String dsr = spans.get(0).html().trim();
-            jdBrand.setValue(dsr);
-            if(spans.get(0).attributes().get("class").equals("level-text-green")){
-                jdBrand.setTrend("低"); //低
-            }else{
-                jdBrand.setTrend("高"); //高
+        try {
+            if(jdBrand.getShopUrl()==null){
+                return jdBrand;
             }
-        } else { //与同行业相比无差别
-            jdBrand.setValue(null);
-            jdBrand.setTrend("--"); //抓取不到分数了
-        }
+            String url = jdBrand.getShopUrl().replace("index", "shopLevel"); //替换链接
+            HtmlPage jdympage = (HtmlPage) wc.getPage(url);
+            String appId = jdympage.querySelector("#pageInstance_appId").getAttributes().getNamedItem("value").getNodeValue();
+            //dsr header dom
+            HtmlPage dsrpage = wc.getPage(dsr_url + appId + "&_=" + new Date().getTime());
+            String jsonText = dsrpage.getBody().asXml();
+            String html = jsonText.replace("  ","").replace("\r\n","").replace("\\&quot;","");
+            Document dom = Jsoup.parse(html);
+            Elements spans = dom.getElementById("jRatingTotal_table").child(2).child(1).getElementsByTag("span");
+            if(spans.size()==2){ //与同行业相比有差别
+                String dsr = spans.get(0).html().trim();
+                jdBrand.setValue(dsr);
+                if(spans.get(0).attributes().get("class").equals("level-text-green")){
+                    jdBrand.setTrend("低"); //低
+                }else{
+                    jdBrand.setTrend("高"); //高
+                }
+            } else { //与同行业相比无差别
+                jdBrand.setValue(null);
+                jdBrand.setTrend("--"); //抓取不到分数了
+            }
+        }catch (Exception e){}
         return jdBrand;
     }
 
